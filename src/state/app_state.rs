49614c598104e812898a04286cec1d5e9d9fdd49
higor-esau <higor.esaut@gmail.com>
2026-05-22 13:01:53 -0400

    adicionando clone

--- src/state/app_state.rs
+++ src/state/app_state.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 
 use crate::models::processed_request::ProcessedRequest;
 
+#[derive(Clone)]
 pub struct AppState{
     pub requests: HashMap<String,ProcessedRequest>
 }
