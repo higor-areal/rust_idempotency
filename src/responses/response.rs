07a7ec1725e4188c92309372f6b06305f72ffc34
higor-esau <higor.esaut@gmail.com>
2026-05-22 13:01:34 -0400

    criando a função response e ignorando a struct antiga

--- src/responses/response.rs
+++ src/responses/response.rs
@@ -1,2 +1,8 @@
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
+};
 
-
+pub fn response(status: StatusCode, msg: String) -> Response {
+    (status, msg).into_response()
+}
