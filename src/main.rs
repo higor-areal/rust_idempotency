808d3046959149dbf4fd8c9844d9eb33ddafde7d
higor-esau <higor.esaut@gmail.com>
2026-05-22 11:36:43 -0400

    iniciando server axum, usando tokio, versão base com state e middleware

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+mod handlers;
+mod responses;
+mod state; 
+mod middleware;
+mod models;
+
+use axum::{
+    Router, 
+    middleware::from_fn_with_state,
+    routing::get
+};
+use tokio::sync::Mutex;
+use std::sync::Arc;
+
+use handlers::payment_handler::{
+    root,
+};
+use middleware::idempotency_middleware::idempotency_middleware;
+
+use state::app_state::AppState;
+
+#[tokio::main]
+async fn main() {
+
+    let state = Arc::new(Mutex::new(AppState::new()));
+
+    let app = Router::new()
+    .route("/", get(root))
+    .route_layer(from_fn_with_state(state, idempotency_middleware))
+    ;
+    
+
+    let listener = match tokio::net::TcpListener::bind("0.0.0.0:3000").await{
+        Ok(tcp)  => tcp,
+        Err(_msg) => return println!("Erro no listener")
+    };
+
+    println!("Start");
+
+    let _server = match axum::serve(listener, app).await{
+        Ok(server) => server,
+        Err(_msg) => return println!("Erro ao criar server axum")
+    };
+
 }
