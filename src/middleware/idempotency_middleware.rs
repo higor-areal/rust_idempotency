f174a01c93d82b96e9c4ee1050d236eed4915b0b
higor-esau <higor.esaut@gmail.com>
2026-05-22 11:32:00 -0400

    adicionando função de middleware de idempotencia, versão base ainda

---
+++ src/middleware/idempotency_middleware.rs
@@ -0,0 +1,52 @@
+use axum::{
+    body::Body, 
+    extract::State, 
+    http::{
+        HeaderMap, Request, StatusCode
+    }, 
+    middleware::Next, 
+    response::{IntoResponse, Response},
+};
+
+use std::sync::Arc;
+use tokio::sync::Mutex;
+
+use crate::state::app_state::AppState;
+
+pub async fn idempotency_middleware(
+    State(state): State<Arc<Mutex<AppState>>>,
+    request: Request<Body>,
+    next: Next,
+) -> Response {
+
+    let key = match get_idempotency_key(request.headers()) {
+        Some(key) => key,
+        None => return bad_request("Idempotency-Key ausente")
+    };
+
+
+    let data = state.lock().await;
+
+    if let Some(res) = data.requests.get(&key) {
+        return (
+            StatusCode::OK,
+            axum::Json(res)
+        ).into_response();
+    }
+
+    next.run(request).await
+}
+
+
+
+fn get_idempotency_key(header: &HeaderMap) -> Option<String> {
+    header
+        .get("Idempotency-Key")?
+        .to_str()
+        .ok()
+        .map(String::from)
+}
+
+fn bad_request(msg: &str) -> Response {
+    (StatusCode::BAD_REQUEST, msg.to_string()).into_response()
+}
