353c2456400b8f1f6a0387a2e84f772637d46572
higor-esau <higor.esaut@gmail.com>
2026-05-24 15:15:29 -0400

    adicionando impl de hash e derive

--- src/models/payment.rs
+++ src/models/payment.rs
@@ -1,7 +1,25 @@
-use serde::{Deserialize};
+use serde::{Deserialize, Serialize};
+use sha2::{Sha256, Digest};
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize, Clone)]
 pub struct Payment{
     client: String,
     amount: f64
+}
+
+impl Payment{
+    pub fn hash(&mut self) -> Option<String>{
+
+        let json = match serde_json::to_string(&self) {
+            Ok(t) => t,
+            Err(_) => return None,
+        };
+        let mut hasher = Sha256::new();
+        
+        hasher.update(json);
+
+        let result = hasher.finalize();
+
+        Some(format!("{:x}", result))
+    }
 }
