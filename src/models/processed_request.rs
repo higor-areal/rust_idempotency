f2477985cb987758868f9f874845970a8fd84d7d
higor-esau <higor.esaut@gmail.com>
2026-05-24 15:16:58 -0400

    Modificando ProcessedRequest, adicionando impl de new, crianod um impl de IntoResponse ao ProcessRequest

--- src/models/processed_request.rs
+++ src/models/processed_request.rs
@@ -1,7 +1,46 @@
 use serde::{Serialize};
+use axum::{
+    response::{IntoResponse, Response},
+    Json,
+    http::StatusCode,
+};
+
+#[derive(Serialize, Clone)]
+pub enum RequestStatus {
+    Processing,
+    Completed,
+}
 
 //essa struct teria a resposta da primeira request
 #[derive(Serialize, Clone)]
-pub struct ProcessedRequest{
-    pub message: String,
+pub struct ProcessedRequest {
+    pub payload_hash: String,
+    pub response: Option<String>,
+    pub status_code: Option<u16>,
+    pub status: RequestStatus,
+}
+
+impl ProcessedRequest{
+    pub fn new(hash: String) -> Option<Self>{
+        
+        Some(
+            ProcessedRequest {
+                payload_hash: hash, 
+                response: None, 
+                status_code: None, 
+                status: RequestStatus::Processing 
+            }
+        )
+    }
+}
+
+impl IntoResponse for ProcessedRequest {
+    fn into_response(self) -> Response {
+        let status = match self.status {
+            RequestStatus::Processing => StatusCode::ACCEPTED,
+            RequestStatus::Completed => StatusCode::OK,
+        };
+
+        (status, Json(self)).into_response()
+    }
 }
