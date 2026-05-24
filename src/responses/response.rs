c20bc9f7b9f061dadcc1adf9dc57cef566ad742c
higor-esau <higor.esaut@gmail.com>
2026-05-24 15:17:42 -0400

    criando função de criando e criando

--- src/responses/response.rs
+++ src/responses/response.rs
@@ -3,6 +3,16 @@ use axum::{
     response::{IntoResponse, Response},
 };
 
+use crate::models::processed_request::ProcessedRequest;
+
 pub fn response(status: StatusCode, msg: String) -> Response {
     (status, msg).into_response()
+}
+
+pub fn response_created(payload: ProcessedRequest) -> Response {
+    (StatusCode::OK, payload).into_response()
+}
+
+pub fn response_creating(payload: ProcessedRequest) -> Response {
+    (StatusCode::CREATED, payload).into_response()
 }
