3592e4c9e89c78117af384701c76554d7bea82a1
higor-esau <higor.esaut@gmail.com>
2026-05-24 15:20:43 -0400

    payment agora edita um ProcessedRequest que foi criado no middleware

--- src/handlers/payment_handler.rs
+++ src/handlers/payment_handler.rs
@@ -7,9 +7,9 @@ use axum::{
 use std::sync::Arc;
 use tokio::sync::Mutex;
 
-use crate::{models::payment::Payment, state::app_state::AppState};
+use crate::{models::payment::Payment, responses::response::response_creating, state::app_state::AppState};
 use crate::responses::response::response;
-use crate::models::processed_request::ProcessedRequest;
+use crate::models::processed_request::{RequestStatus};
 
 
 pub async fn root() -> &'static str{
@@ -24,14 +24,19 @@ pub async fn payment(
 
     let msg = "Pagamento processado".to_string();
 
-    let processed_request = ProcessedRequest{
-        message: msg.clone()
-    };
-    
+    //aqui nos vamos somente editar o campo de response
+
     let mut data = state.lock().await;
 
-    data.requests.insert(key.clone(), processed_request);
+    let process = match data.requests.get_mut(&key){
+        Some(t) => t,
+        None => return response(StatusCode::BAD_REQUEST, "Erro ao buscar key".to_string())
+    };
 
-    response(StatusCode::CREATED, msg)
+    process.response = Some(msg);
+    process.status = RequestStatus::Completed;
+    process.status_code = Some(StatusCode::CREATED.as_u16());
+
+    response_creating(process.clone())
 
 }
